613b96752d96a5b1877bf12ebc7b686e3e01e0d3
dhthanh1997 <74946734+dhthanh1997@users.noreply.github.com>
2023-01-12 17:05:54 +0700

    12/01/2023 Thành update dashboard (icon) + task-management (sidebar)

--- task-management/src/app/@theme/sidebar/sidebar.component.ts
+++ task-management/src/app/@theme/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { debug } from 'console';
 import { ShareService } from '../shared/share.service';
 
 @Component({
@@ -7,16 +8,26 @@ import { ShareService } from '../shared/share.service';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-
   isCollapsed = false;
-  
-  constructor(private shareService: ShareService) {
+
+  isActive = false;
+
+  constructor(private shareService: ShareService, private element: ElementRef) {
     // do something
   }
 
   ngOnInit(): void {
     // do something
-    
-    
+  }
+
+  linkActive(e: any) {
+    if (
+      this.element.nativeElement.querySelector('#navList a.active') !== null
+    ) {
+      this.element.nativeElement
+        .querySelector('#navList a.active')
+        .classList.remove('active');
+    }
+    e.target.className = 'nav-link active';
   }
 }
