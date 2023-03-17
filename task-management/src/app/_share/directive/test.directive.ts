7b4c071ac3e0e7cc8a2d55bf61245c6e13fbd5ff
DESKTOP-QG1OPQ5\admin <huuthanh1308@gmail.com>
2023-03-17 19:14:34 +0700

    17/03/2023 Thành update

--- task-management/src/app/_share/directive/test.directive.ts
+++ task-management/src/app/_share/directive/test.directive.ts
@@ -9,16 +9,16 @@ export class TestDirective implements AfterViewChecked {
   constructor(private element: ElementRef, private shareService: ShareService) { }
 
   ngAfterViewChecked(): void {
-    this.shareService.isTest.subscribe({
-      next: (res) => {
-        if (res) {
-          this.Color(res);
-        }
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    })
+    // this.shareService.isTest.subscribe({
+    //   next: (res) => {
+    //     if (res) {
+    //       this.Color(res);
+    //     }
+    //   },
+    //   error: (err) => {
+    //     console.log(err);
+    //   }
+    // })
   }
 
   private Color(value: string) {
