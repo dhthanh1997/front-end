c62c32707f8bd1ee510b89f75b202c159fe4bf16
DESKTOP-QG1OPQ5\admin <huuthanh1308@gmail.com>
2023-04-03 08:14:47 +0700

    03/04/2023 Thành update

--- task-management/src/app/pages/project/project-timeline/project-timeline.component.ts
+++ task-management/src/app/pages/project/project-timeline/project-timeline.component.ts
@@ -5,13 +5,53 @@ import HC_dragg from 'highcharts/modules/draggable-points';
 import { ProjectData } from 'src/app/_core/api/project/project-data';
 HC_dragg(Highcharts);
 
+Highcharts.setOptions({
+  lang: {
+    months: [
+      'Tháng 1',
+      'Tháng 2',
+      'Tháng 3',
+      'Tháng 4',
+      'Tháng 5',
+      'Tháng 6',
+      'Tháng 7',
+      'Tháng 8',
+      'Tháng 9',
+      'Tháng 10',
+      'Tháng 11',
+      'Tháng 12',
+    ],
+    shortMonths: [
+      'T1',
+      'T2',
+      'T3',
+      'T4',
+      'T5',
+      'T6',
+      'T7',
+      'T8',
+      'T9',
+      'T10',
+      'T11',
+      'T12',
+    ],
+    weekdays: [
+      'Thứ 2 ', 'Thứ 3', 'Thứ 4', 'Thứ 5', 'Thứ 6', 'Thứ 7', 'Chủ nhật'
+    ],
+    accessibility: {
+      series: {
+
+      }
+    }
+  },
+});
+
 @Component({
   selector: 'app-project-timeline',
   templateUrl: './project-timeline.component.html',
   styleUrls: ['./project-timeline.component.scss'],
 })
 export class ProjectTimelineComponent implements OnInit {
-
   Highcharts: typeof Highcharts = Highcharts;
   chartOptions: any;
   loading = true;
@@ -24,7 +64,10 @@ export class ProjectTimelineComponent implements OnInit {
   public series: any[] = [];
   public data: any[] = [];
 
-  constructor(private projectData: ProjectData, private route: ActivatedRoute) {}
+  constructor(
+    private projectData: ProjectData,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.getSubProject();
@@ -43,91 +86,98 @@ export class ProjectTimelineComponent implements OnInit {
   }
 
   getSubProject() {
+    this.projectData
+      .search(1, 999, `parentId.eq.${this.getIdProject()},`)
+      .subscribe({
+        next: (res) => {
+          if (res) {
+            // debugger;
+            console.log(res);
+            this.subProjectList = res.pagingData.content;
 
-    this.projectData.search(1, 999, `parentId.eq.${this.getIdProject()},`).subscribe({
-      next: (res) => {
-        if (res) {
-          // debugger;
-          console.log(res);
-          this.subProjectList = res.pagingData.content;
+            let dataName: any[] = [];
 
-          let dataName = []
+            for (let i = 0; i < this.subProjectList.length; i++) {
+              this.data.push({
+                id: i,
+                name: this.subProjectList[i].name,
+                start: new Date(this.subProjectList[i].startDate).getTime(),
+                end: new Date(this.subProjectList[i].endDate).getTime(),
+                y: i,
+              });
+              dataName.push(this.subProjectList[i].name);
+            }
 
-          for (let i = 0; i < this.subProjectList.length; i++) {
-            this.data.push({
-              id: i,
-              name: this.subProjectList[i].name,
-              start: (new Date(this.subProjectList[i].startDate)).getTime(),
-              end: (new Date(this.subProjectList[i].endDate)).getTime(),
-              y: i,
-            });
-            dataName.push(this.subProjectList[i].name);
+            const timezone = new Date().getTimezoneOffset();
+
+            this.chartOptions = {
+              chart: {
+                type: 'gantt',
+              },
+
+              title: {
+                text: 'Dự án: ' + this.getNameProject(),
+              },
+
+              global: {
+                timezoneOffset: timezone,
+              },
+
+              tooltip: {
+                formatter: function () {
+                  for(let i = 0; i < dataName.length; i++) {
+                    return 'Tên: ' + dataName[i].name;
+                  }
+                  return 0;
+                }
+              },
+
+              plotOptions: {
+                series: {
+                  animation: false, // Do not animate dependency connectors
+                  dataLabels: {
+                    enabled: true,
+                    format: '{point.name}',
+                    style: {
+                      cursor: 'default',
+                      pointerEvents: 'none',
+                    },
+                  },
+                  allowPointSelect: true,
+                  point: {
+                    events: {
+                      select: this.updateRemoveButtonStatus,
+                      unselect: this.updateRemoveButtonStatus,
+                      remove: this.updateRemoveButtonStatus,
+                    },
+                  },
+                },
+              },
+
+              xAxis: {
+                minPadding: 0.2,
+                maxPadding: 0.2,
+              },
+
+              yAxis: {
+                type: 'category',
+                categories: dataName,
+              },
+
+              series: [
+                {
+                  data: this.data,
+                },
+              ],
+            };
+
+            this.loading = false;
           }
-
-          this.chartOptions = {
-            chart: {
-              type: 'gantt',
-            },
-
-            title: {
-              text: this.getNameProject(),
-            },
-
-            lang: {
-              accessibility: {
-                axis: {
-                  xAxisDescriptionPlural:
-                    'The chart has a two-part X axis showing time in both week numbers and days.',
-                },
-              },
-            },
-
-            plotOptions: {
-              series: {
-                animation: false, // Do not animate dependency connectors
-                dataLabels: {
-                  enabled: true,
-                  format: '{point.name}',
-                  style: {
-                    cursor: 'default',
-                    pointerEvents: 'none',
-                  },
-                },
-                allowPointSelect: true,
-                point: {
-                  events: {
-                    select: this.updateRemoveButtonStatus,
-                    unselect: this.updateRemoveButtonStatus,
-                    remove: this.updateRemoveButtonStatus,
-                  },
-                },
-              },
-            },
-
-            xAxis: {
-              minPadding: 0.2,
-              maxPadding: 0.2,
-            },
-
-            yAxis: {
-              type: 'category',
-              categories: dataName,
-            },
-
-            series: [
-              {
-                data: this.data,
-              },
-            ],
-          };
-
-          this.loading = false;
-        }
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 
   updateRemoveButtonStatus() {
