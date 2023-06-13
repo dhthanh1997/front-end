78b7c316f3c8240bb519ecf1e405dc5341de49ad
LAPTOP-2CP7E18J\ACER <swordartonline1308@gmail.com>
2023-06-13 17:00:37 +0700

    13/06/2023 Thanh update

--- task-management/src/environments/environment.prod.ts
+++ task-management/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'http://host.docker.internal:8098/taskManagement/api',
+  // apiUrl:'http://host.docker.internal:8098/taskManagement/api',
   // loginUrl: 'http://localhost:4200',
-  gatewayUrl:'http://host.docker.internal:8055',
+  apiUrl:'http://host.docker.internal:8055',
 };
--- task-management/src/environments/environment.staging.ts
+++ task-management/src/environments/environment.staging.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl:'http://host.docker.internal:8098/taskManagement/api',
-  loginUrl: 'http://host.docker.internal:5200'
+  // loginUrl: 'http://host.docker.internal:5200',
+  gatewayUrl:'http://host.docker.internal:8055',
 };
