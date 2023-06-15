8536763e1328a701da6601ddb4903e3432c09bb8
LAPTOP-2CP7E18J\ACER <swordartonline1308@gmail.com>
2023-06-15 08:59:40 +0700

    up

--- task-management/src/environments/environment.prod.ts
+++ task-management/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'http://10.2.6.142:8055/taskManagement/api',
+  apiUrl:'http://host.docker.internal:8055/taskManagement/api',
   // loginUrl: 'http://localhost:4200',
-  gatewayUrl:'http://10.2.6.142:8055',
+  gatewayUrl:'http://host.docker.internal:8055',
 };
