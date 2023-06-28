75a641b83ad4ac7ea0414fd6526cb23c0596956d
LAPTOP-2CP7E18J\ACER <swordartonline1308@gmail.com>
2023-06-28 11:57:48 +0700

    up

--- task-management/src/environments/environment.prod.ts
+++ task-management/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'http://10.2.6.142:8092/taskManagement/api',
-  loginUrl: 'http://localhost:4200'
+  apiUrl:'http://10.2.6.147:8055/taskManagement/api',
+  // loginUrl: 'http://localhost:4200'
+  gatewayUrl:'http://10.2.6.147:8055',
 };
